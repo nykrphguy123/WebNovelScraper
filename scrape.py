9679b2434ef1f69ddc791d1289166b62fff9887c
chuu <kyaktor@keemail.me>
2019-07-14 12:43:33 -0400

    funtionality

--- scrape.py
+++ scrape.py
@@ -7,24 +7,51 @@
 import requests
 import urllib.request
 import time
+import re
+import os
 from bs4 import BeautifulSoup
 
-print('input URL to webnovel')
-novel = input()
-url = novel
+number_of_chapters = []
+volume_number = []
 
-response = requests.get(url)
+num = 0
+bad_chars = 'volume'
+table=str.maketrans('','',bad_chars)
+
+os.system('clear')
+
+print('Enter URL to novel')
+novel = input()
+full_link = novel
+
+novel_ID = full_link[46:]
+response = requests.get(full_link)
 
 soup = BeautifulSoup(response.text, "html.parser")
-#soup = BeautifulSoup(response.text, "html.parser")
 
-#soup.findAll('p')
-#one_p_tag = soup.findAll('p')
-one_p_tag = soup.findAll('p')[0].next
+for link in soup.findAll('li'):
+    number_of_chapters.append(link.get('id'))
 
-with open("Output.txt", "w") as text_file:
-    print(one_p_tag, file=text_file)
+for value in number_of_chapters:
+    if value != None:
+        volume_number.append(value)
 
+number_of_chapters = len(volume_number)
+recent_volume = volume_number[0].translate(table)
+volume_number_int = int(recent_volume)
 
+os.system('clear')
+print('Fetching ' + str(number_of_chapters) + ' chapters..')
+print('Novel ID: ' + novel_ID)
 
+while num < volume_number_int+1:
+    num += 1
+    link = 'https://novel.naver.com/best/detail.nhn?novelId=' + novel_ID + '&volumeNo=' + str(num)
+    second_response = requests.get(link)
+    soup2 = BeautifulSoup(second_response.text, 'html.parser')
+    one_p_tag = soup2.findAll('p')[0].next
+    with open('webnovel'+ str(num) + '.txt', 'w') as text_file:
+        print(one_p_tag, file=text_file)
 
+os.system('clear')
+print('Scraped ' + str(number_of_chapters) + ' chapters!')
